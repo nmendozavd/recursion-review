cbf07c2d7575a4ff423425ff6cf4b46b18e15f6a
Noel Mendoza <nmendozavd@gmail.com>
2019-12-10 00:38:58 -0800

    added psuedo code

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,20 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+    
+    // create empty array to push nodes
+    var nodes = [];
+    
+
+    var searchNodes = function(node){
+        // compare nodes classname with classname
+            // if matched, save node
+        
+        // iterate over children
+            // for each child, invoke getElementsByClassName 
+    // start with top of document
+    searchNodes(document.body);
+
+    return nodes;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,47 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // edge case
+  if (obj === null) {
+    return 'null';
+  }
+  // if string
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  // if number or boolean
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return '' + obj + '';
+  }
+  // if function or undefined 
+  if (typeof obj === 'function' || typeof obj === 'undefined' ) {
+    return '';
+  }
+  // if array
+  if (Array.isArray(obj)) {
+    var stringArr = [];
+
+    // helper function forEach iterates over each element
+    obj.forEach(function (element) {
+      stringArr.push(stringifyJSON(element));
+    });
+    return '[' + stringArr.toString() + ']';
+  }
+
+  // if object
+  if (typeof obj === 'object') {
+    // create empty string object
+    // create variable to grab all keys in obj
+
+    // helper function for each 
+    // typeof edge cases for keys and values undefined and function 
+    // return empty string if any are true
+    // otherwise all to string object stringify key and value format
+
+    // return in object string
+  }
+
+  
+  // return obj using toString();
+
 };
