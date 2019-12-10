d129117ff79faca1155bdb71e57ab4ffe9186940
nmendozavd <nmendozavd@gmail.com>
2019-12-10 10:34:34 -0800

    completed recursion review

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -16,7 +16,7 @@ var stringifyJSON = function(obj) {
   if (typeof obj === 'number' || typeof obj === 'boolean') {
     return '' + obj + '';
   }
-  // if function or undefined 
+  // if function or undefined
   if (typeof obj === 'function' || typeof obj === 'undefined' ) {
     return '';
   }
@@ -28,23 +28,33 @@ var stringifyJSON = function(obj) {
     obj.forEach(function (element) {
       stringArr.push(stringifyJSON(element));
     });
-    return '[' + stringArr.toString() + ']';
+    return '[' + stringArr + ']';
   }
 
   // if object
   if (typeof obj === 'object') {
     // create empty string object
+    var stringObj = '';
+
     // create variable to grab all keys in obj
+    var objKeys = Object.keys(obj);
 
-    // helper function for each 
-    // typeof edge cases for keys and values undefined and function 
-    // return empty string if any are true
-    // otherwise all to string object stringify key and value format
-
-    // return in object string
+    // helper function for each
+    objKeys.forEach (function (k) {
+      // typeof edge cases for keys and values undefined and function
+      if (typeof k === 'function' || typeof k === undefined || typeof obj[k] === 'function' || typeof obj[k] === 'undefined') {
+        return '';
+      } else {
+      // otherwise all to string object stringify key and value format
+        stringObj += stringifyJSON(k) + ':' + stringifyJSON(obj[k]) + ',';
+      }
+    });
+    // return in stringobject using slice for stringobj length
+    return '{' + stringObj.slice(0, stringObj.length - 1) + '}';
   }
+  // return obj
+  return obj;
 
-  
-  // return obj using toString();
+
 
 };
